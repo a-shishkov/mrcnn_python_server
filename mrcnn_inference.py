1bb20d2d47633b39448ee7e4c2c301997f8d348b
yomi-um <aleksandr.shishkov@student.um.si>
2021-12-28 10:26:17 +0100

    first commit

---
+++ mrcnn_inference.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import skimage.io
+
+ROOT_DIR = os.path.abspath("./Mask_RCNN/")
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+from mrcnn import visualize
+
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+CAR_PARTS_MODEL_DIR = "/car_parts_models"
+MODEL_PATH = os.path.join(ROOT_DIR, "models/car_parts_smallest_fixed_anno.h5")
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+class CarPartsConfigSmallest(Config):
+    NAME = "car_part"
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 9
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+
+class_names_car_parts_smallest = ['BG', 'bumper', 'glass', 'door',
+                                  'light', 'hood', 'mirror', 'trunk', 'wheel']
+
+class MRCNN:
+    def __init__(self, mode, config):
+        self.config = config
+        self.model = modellib.MaskRCNN(mode=mode, model_dir=MODEL_DIR, config=self.config)
+        self.model.load_weights(MODEL_PATH, by_name=True)
+
+    def detect(self, image_path):
+        self.image = skimage.io.imread(image_path)
+        self.results = self.model.detect([self.image])
+        return self.results
+
+    def visualize(self, results, filename):
+        r = results[0]
+        visualize.display_instances(self.image, r['rois'], r['masks'], r['class_ids'], 
+        class_names_car_parts_smallest, r['scores'], show_img=False, filename=filename)
---
+++ multi_connection_server.py
@@ -0,0 +1,94 @@
+import json
+import socket
+import os
+from _thread import *
+import mrcnn_inference
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+
+HOST = s.getsockname()[0]
+PORT = 65432
+s.close()
+
+config = mrcnn_inference.CarPartsConfigSmallest()
+model = mrcnn_inference.MRCNN('inference', config)
+
+
+def send_message(conn, message):
+    conn.send(len(message).to_bytes(4, "big"))
+    conn.sendall(message)
+
+
+def client_thread(conn, addr):
+    try:
+        while True:
+            data = conn.recv(4)
+            if data:
+                msg_len = int.from_bytes(data, "big")
+                print("Client image size", msg_len)
+                filename = 'img_{}.jpg'.format(''.join(addr[0].split('.')))
+                f = open(filename, 'wb')
+                for i in range(msg_len):
+                    data = conn.recv(1)
+                    if not data:
+                        break
+                    f.write(data)
+
+                f.close()
+
+                send_message(conn, json.dumps(
+                    {"response": "Downloaded"}).encode('utf-8'))
+
+                results = model.detect(filename)
+
+                filename_out = 'img_{}_out.jpg'.format(''.join(addr[0].split('.')))
+                model.visualize(results, filename_out)
+
+                r = results[0]
+                r.pop('masks')
+                for key in r:
+                    r[key] = r[key].tolist()
+                r["response"] = "NoMasksResults"
+
+                result = json.dumps(r).encode('utf-8')
+
+                print("Result size", len(result))
+                send_message(conn, json.dumps(
+                    {"response": "Sending", "size": len(result)}).encode('utf-8'))
+
+                send_message(conn, result)
+                print("Sent results")
+
+                filesize = os.path.getsize(filename_out)
+                print('Out file size', filesize)
+                conn.send(filesize.to_bytes(4, 'big'))
+
+                f = open(filename_out, 'rb')
+                for i in range(filesize):
+                    conn.send(f.read(1))
+                f.close()
+
+                print("Sent out image")
+            else:
+                break
+    except ConnectionResetError as e:
+        print('exception occured', e)
+    print('exit', addr)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.bind((HOST, PORT))
+    print(s.getsockname())
+except socket.error as e:
+    print(str(e))
+
+print('Waiting for a connection...')
+s.listen()
+
+while True:
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    start_new_thread(client_thread, (conn, addr,))
+s.close()
