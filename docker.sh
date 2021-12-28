e5a6074ec7d9e0ad6b72d50eea6b2fcd495f53d0
a-shishkov <aleksandr.shishkov@result.si>
2021-12-28 14:50:44 +0100

    Removed sudo from script

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
-sudo docker build -t mrcnn_server .
-sudo docker rm -f server
-sudo docker run --network host --name server mrcnn_server
+docker build -t mrcnn_server .
+docker rm -f server
+docker run --network host --name server mrcnn_server
