5bcb46b8969a726f61f928ce57a34b39057f2f47
a-shishkov <aleksandr.shishkov@result.si>
2022-02-03 18:27:14 +0100

    improves

--- mrcnn_inference.py
+++ mrcnn_inference.py
@@ -4,9 +4,9 @@ import skimage.io
 
 ROOT_DIR = os.path.abspath("./Mask_RCNN/")
 sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn.config import Config
-from mrcnn import model as modellib, utils
 from mrcnn import visualize
+from mrcnn import model as modellib, utils
+from mrcnn.config import Config
 
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
@@ -14,6 +14,7 @@ CAR_PARTS_MODEL_DIR = "/car_parts_models"
 # MODEL_PATH = os.path.join(ROOT_DIR, "models/car_parts_smallest_fixed_anno.h5")
 # Directory of images to run detection on
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
 
 class CarPartsConfigSmallest(Config):
     NAME = "car_part"
@@ -24,8 +25,6 @@ class CarPartsConfigSmallest(Config):
     IMAGE_MAX_DIM = 512
     RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
 
-class_names_car_parts_smallest = ['BG', 'bumper', 'glass', 'door',
-                                  'light', 'hood', 'mirror', 'trunk', 'wheel']
 
 class CarDamageConfig(Config):
     NAME = "car_damage"
@@ -36,12 +35,17 @@ class CarDamageConfig(Config):
     IMAGE_MAX_DIM = 512
     RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
 
-class_names_car_damage = ['BG', 'damage']
+
+class_names = {'parts': ['BG', 'bumper', 'glass', 'door',
+                         'light', 'hood', 'mirror', 'trunk', 'wheel'],
+               'damage': ['BG', 'damage']}
+
 
 class MRCNN:
     def __init__(self, mode, config, model_path):
         self.config = config
-        self.model = modellib.MaskRCNN(mode=mode, model_dir=MODEL_DIR, config=self.config)
+        self.model = modellib.MaskRCNN(
+            mode=mode, model_dir=MODEL_DIR, config=self.config)
         self.model.load_weights(model_path, by_name=True)
 
     def detect(self, image_path):
@@ -51,5 +55,5 @@ class MRCNN:
 
     def visualize(self, results, filename, class_names):
         r = results[0]
-        visualize.display_instances(self.image, r['rois'], r['masks'], r['class_ids'], 
-        class_names, r['scores'], show_img=False, filename=filename)
+        visualize.display_instances(self.image, r['rois'], r['masks'], r['class_ids'],
+                                    class_names, r['scores'], show_img=False, filename=filename)
--- multi_connection_server.py
+++ multi_connection_server.py
@@ -1,103 +1,111 @@
 import json
 import socket
 import os
+import base64
 from _thread import *
 import mrcnn_inference
 
+
 def read_message(conn):
+    BUFF_SIZE = 4096
     data = conn.recv(4)
     if data:
-        msg_len = int.from_bytes(data, "big")
-        print("Message size", msg_len)
-        return conn.recv(msg_len)
+        msg_len = int.from_bytes(data, 'big')
+        data = b''
+        print('Message size', msg_len)
+        while len(data) < msg_len:
+            packet = conn.recv(msg_len-len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
 
 
 def send_message(conn, message):
-    conn.send(len(message).to_bytes(4, "big"))
+    conn.send(len(message).to_bytes(4, 'big'))
     conn.sendall(message)
 
 
 def client_thread(conn, addr):
-
     try:
         while True:
-            model_type = read_message(conn)
-            print(model_type)
-            data = conn.recv(4)
-            if data:
-                msg_len = int.from_bytes(data, "big")
-                print("Client image size", msg_len)
-                filename = 'img_{}.jpg'.format(''.join(addr[0].split('.')))
-                f = open(filename, 'wb')
-                for i in range(msg_len):
-                    data = conn.recv(1)
-                    if not data:
-                        break
-                    f.write(data)
-
-                f.close()
-
-                send_message(conn, json.dumps(
-                    {"response": "Downloaded"}).encode('utf-8'))
-
-                if model_type == b'damage':
-                    results = damage_model.detect(filename)
-                elif model_type == b'parts':
-                    results = car_parts_model.detect(filename)
-
-                filename_out = 'img_{}_out.jpg'.format(''.join(addr[0].split('.')))
-
-                if model_type == b'damage':
-                    damage_model.visualize(results, filename_out, mrcnn_inference.class_names_car_damage)
-                elif model_type == b'parts':
-                    car_parts_model.visualize(results, filename_out, mrcnn_inference.class_names_car_parts_smallest)
-
-                r = results[0]
-                r.pop('masks')
-                for key in r:
-                    r[key] = r[key].tolist()
-                r["response"] = "NoMasksResults"
-
-                result = json.dumps(r).encode('utf-8')
-
-                print("Result size", len(result))
-                send_message(conn, json.dumps(
-                    {"response": "Sending", "size": len(result)}).encode('utf-8'))
-
-                send_message(conn, result)
-                print("Sent results")
-
-                filesize = os.path.getsize(filename_out)
-                print('Out file size', filesize)
-                conn.send(filesize.to_bytes(4, 'big'))
-
-                f = open(filename_out, 'rb')
-                for i in range(filesize):
-                    conn.send(f.read(1))
-                f.close()
-
-                print("Sent out image")
-            else:
+            client_msg = read_message(conn)
+            if client_msg is None:
                 break
+            client_msg = json.loads(client_msg)
+            print(client_msg['annotations'])
+
+            filename = 'img_{}.jpg'.format(''.join(addr[0].split('.')))
+            with open(filename, 'wb') as f:
+                f.write(base64.b64decode(client_msg['image']))
+
+            model_type = client_msg['model']
+
+            send_message(conn, json.dumps(
+                {'response': 'Message', 'message': 'Running model'}).encode('utf-8'))
+
+            results = models[model_type].detect(filename)
+
+            if not results[0]['rois'].shape[0]:
+                send_message(conn, json.dumps(
+                    {'response': 'No results'}).encode('utf-8'))
+                print('no results')
+                continue
+
+            filename_out = 'img_{}_out.jpg'.format(''.join(addr[0].split('.')))
+
+            models[model_type].visualize(
+                results, filename_out, mrcnn_inference.class_names[model_type])
+
+            r = results[0]
+            r.pop('masks')
+            for key in r:
+                r[key] = r[key].tolist()
+
+            filesize = os.path.getsize(filename_out)
+            print('Out file size', filesize)
+
+            with open(filename_out, 'rb') as f:
+                img = f.read()
+
+            r['image'] = base64.b64encode(img).decode('utf-8')
+            r['response'] = 'Results'
+
+            result = json.dumps(r).encode('utf-8')
+
+            print('Result size', len(result))
+            send_message(conn, json.dumps(
+                {'response': 'Message', 'message': 'Sending {} bytes'.format(len(result))}).encode('utf-8'))
+
+            # print('img', list(img))
+            print('Img size', len(json.dumps(r['image']).encode('utf-8')))
+            send_message(conn, result)
+            print('Sent results')
+
     except ConnectionResetError as e:
         print('exception occured', e)
     print('exit', addr)
-
+    return
 
 try:
     PORT = 65432
-    print("Socket connect")
+    print('Socket connect')
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", PORT))
+    s.connect(('8.8.8.8', PORT))
     HOST = s.getsockname()[0]
-    print("Socket connected", HOST)
+    print('Socket connected', HOST)
     s.close()
 
     car_parts_config = mrcnn_inference.CarPartsConfigSmallest()
-    car_parts_model = mrcnn_inference.MRCNN('inference', car_parts_config, "Mask_RCNN/models/car_parts_smallest_fixed_anno.h5")
 
     damage_config = mrcnn_inference.CarDamageConfig()
-    damage_model = mrcnn_inference.MRCNN('inference', damage_config, "Mask_RCNN/models/car_damage.h5")
+
+    models = {'parts': mrcnn_inference.MRCNN('inference',
+                                             car_parts_config,
+                                             'Mask_RCNN/models/car_parts_smallest_fixed_anno.h5'),
+              'damage': mrcnn_inference.MRCNN('inference',
+                                              damage_config,
+                                              'Mask_RCNN/models/car_damage.h5')}
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
