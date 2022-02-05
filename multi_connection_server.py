7618bf0cae28be5e7ac4e6736e7ffabeb08eccc1
a-shishkov <aleksandr.shishkov@result.si>
2022-02-05 03:31:18 +0100

    Implemented annotation and images saving

--- multi_connection_server.py
+++ multi_connection_server.py
@@ -26,6 +26,44 @@ def send_message(conn, message):
     conn.sendall(message)
 
 
+class ClientData:
+    def __init__(self, ip, model):
+        self.dirname = os.path.join(
+            'results', ip.replace('.', '_'), model)
+        self.dirname_in = os.path.join(
+            self.dirname, 'input')
+        self.dirname_out = os.path.join(
+            self. dirname, 'output')
+
+        if not os.path.isdir(self.dirname_in):
+            os.makedirs(self.dirname_in)
+        if not os.path.isdir(self.dirname_out):
+            os.makedirs(self.dirname_out)
+
+        self.annotations_path = os.path.join(self.dirname, 'annotations.json')
+
+    def current_id(self):
+        return len(os.listdir(self.dirname_in))
+
+
+class Image:
+    def __init__(self, name, folder):
+        self.name = name
+        self.path = os.path.join(folder, name)
+
+    def size(self):
+        return human_readable_size(
+            os.path.getsize(self.path))
+
+
+def human_readable_size(size, decimal_places=0):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f} {unit}"
+
+
 def client_thread(conn, addr):
     try:
         while True:
@@ -33,18 +71,40 @@ def client_thread(conn, addr):
             if client_msg is None:
                 break
             client_msg = json.loads(client_msg)
-            print(client_msg['annotations'])
 
-            filename = 'img_{}.jpg'.format(''.join(addr[0].split('.')))
-            with open(filename, 'wb') as f:
-                f.write(base64.b64decode(client_msg['image']))
+            client_data = ClientData(addr[0], client_msg['model'])
 
-            model_type = client_msg['model']
+            image_id = client_data.current_id()
+            input_image = Image(f'{image_id}.jpg', client_data.dirname_in)
+
+            if client_msg['annotations'] is not None:
+                image_annotations = client_msg['annotations']
+
+                if os.path.exists(client_data.annotations_path):
+                    with open(client_data.annotations_path, 'r') as anno_file:
+                        annotations_data = json.load(anno_file)
+                else:
+                    annotations_data = {'annotations': [], 'images': []}
+
+                last_anno_id = len(annotations_data['annotations'])
+                for annotation in image_annotations:
+                    annotation['image_id'] = image_id
+                    annotation['id'] = last_anno_id
+                    last_anno_id += 1
+                    annotations_data['annotations'].append(annotation)
+                annotations_data['images'].append(
+                    {'file_name': input_image.name, 'id': image_id})
+                with open(client_data.annotations_path, 'w') as anno_file:
+                    json.dump(annotations_data, anno_file,
+                              indent=4, sort_keys=True)
+
+            with open(input_image.path, 'wb') as image_file:
+                image_file.write(base64.b64decode(client_msg['image']))
 
             send_message(conn, json.dumps(
                 {'response': 'Message', 'message': 'Running model'}).encode('utf-8'))
 
-            results = models[model_type].detect(filename)
+            results = models[client_msg['model']].detect(input_image.path)
 
             if not results[0]['rois'].shape[0]:
                 send_message(conn, json.dumps(
@@ -52,30 +112,30 @@ def client_thread(conn, addr):
                 print('no results')
                 continue
 
-            filename_out = 'img_{}_out.jpg'.format(''.join(addr[0].split('.')))
+            output_image = Image(f'{image_id}_out.jpg',
+                                 client_data.dirname_out)
 
-            models[model_type].visualize(
-                results, filename_out, mrcnn_inference.class_names[model_type])
+            models[client_msg['model']].visualize(
+                results, output_image.path, mrcnn_inference.class_names[client_msg['model']])
 
             r = results[0]
             r.pop('masks')
             for key in r:
                 r[key] = r[key].tolist()
 
-            filesize = os.path.getsize(filename_out)
-            print('Out file size', filesize)
+            print('Out file size', output_image.size())
 
-            with open(filename_out, 'rb') as f:
-                img = f.read()
+            with open(output_image.path, 'rb') as image_file:
+                r['image'] = base64.b64encode(
+                    image_file.read()).decode('utf-8')
 
-            r['image'] = base64.b64encode(img).decode('utf-8')
             r['response'] = 'Results'
 
             result = json.dumps(r).encode('utf-8')
 
             print('Result size', len(result))
             send_message(conn, json.dumps(
-                {'response': 'Message', 'message': 'Sending {} bytes'.format(len(result))}).encode('utf-8'))
+                {'response': 'Message', 'message': f'Receiving {human_readable_size(len(result))}'}).encode('utf-8'))
 
             # print('img', list(img))
             print('Img size', len(json.dumps(r['image']).encode('utf-8')))
@@ -87,6 +147,7 @@ def client_thread(conn, addr):
     print('exit', addr)
     return
 
+
 try:
     PORT = 65432
     print('Socket connect')
@@ -97,9 +158,7 @@ try:
     s.close()
 
     car_parts_config = mrcnn_inference.CarPartsConfigSmallest()
-
     damage_config = mrcnn_inference.CarDamageConfig()
-
     models = {'parts': mrcnn_inference.MRCNN('inference',
                                              car_parts_config,
                                              'Mask_RCNN/models/car_parts_smallest_fixed_anno.h5'),
